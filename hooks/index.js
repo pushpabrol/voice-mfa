8d310cd9e25e82b9fff9161062614394753c98ac
pushpabrol <pushp.abrol@gmail.com>
2016-11-02 11:27:19 -0400

    Changes for hook again 4

--- hooks/index.js
+++ hooks/index.js
@@ -29,13 +29,6 @@ function validateJwt(path) {
   }
 }
 
-hooks.use(function(req, res, next) {
-  auth0({
-    scopes: req.webtaskContext.data.AUTH0_SCOPES,
-    clientId: req.webtaskContext.data.AUTH0_CLIENT_ID,
-    rootTenantAuthority: 'https://' + req.webtaskContext.data.AUTH0_DOMAIN
-  })(req, res, next)
-});
 
 // Validate JWT for on-install
 hooks.use('/on-install', validateJwt('/.extensions/on-install'));
