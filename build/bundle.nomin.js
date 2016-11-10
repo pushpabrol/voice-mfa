880e126bfc21048a8ed2ff687b160618162a8c76
pushpabrol <pushp.abrol@gmail.com>
2016-11-10 16:19:47 -0500

    code changes

--- build/bundle.nomin.js
+++ build/bundle.nomin.js
@@ -641,7 +641,7 @@ module.exports =
 	  var svcAccount = {
 	    projectId: vars.FB_PROJECT_ID,
 	    clientEmail: vars.FB_CLIENT_EMAIL,
-	    privateKey: new Buffer(vars.FB_PRIVATE_KEY, 'base64').toString('utf8').replace(/\\n/g, '\n')
+	    privateKey: vars.FB_PRIVATE_KEY.replace(/\\n/g, '\n')
 	  }
 	
 	  console.log(svcAccount);
